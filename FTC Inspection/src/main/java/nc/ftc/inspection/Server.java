9e8e9a7cec8c8c797ea0da3c0be1093a789663b4
Trey Woodlief <adwiii7@gmail.com>
2017-08-02 23:19:02 -0400

    added manual1 for 2018

--- FTC Inspection/src/main/java/nc/ftc/inspection/Server.java
+++ FTC Inspection/src/main/java/nc/ftc/inspection/Server.java
@@ -5,6 +5,10 @@ import static spark.debug.DebugScreen.*;
 
 public class Server {
 	public static void main(String[] args) {
+		port(80);
+		staticFiles.location("/public");
 		
+		get("*", (req, res) -> "this is different");
 	}
 }
+
