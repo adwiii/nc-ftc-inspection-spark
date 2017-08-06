01d074b3e5b131488d5ed8615129fc2ba7b493a5
Thomas Barnette <tvbarnet@ncsu.edu>
2017-08-06 14:37:17 -0400

    Added Users DAO

--- FTC Inspection/src/main/java/nc/ftc/inspection/Server.java
+++ FTC Inspection/src/main/java/nc/ftc/inspection/Server.java
@@ -3,8 +3,13 @@ package nc.ftc.inspection;
 import static spark.Spark.*;
 import static spark.debug.DebugScreen.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 
+import nc.ftc.inspection.dao.UsersDAO;
 import nc.ftc.inspection.spark.util.Filters;
 import nc.ftc.inspection.spark.util.Path;
 import nc.ftc.inspection.spark.util.ViewUtil;
@@ -13,7 +18,14 @@ import spark.template.velocity.*;
 import spark.Route;
 
 public class Server {
+	public static final String GLOBAL_DB = "jdbc:sqlite:src/main/resources/db/global.db"; 
 	public static void main(String[] args) {
+		try {//idk, somethings up with gradle but this makes it work.
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		
 		port(80);
 		staticFiles.location("/public");
 		enableDebugScreen();
---
+++ FTC Inspection/src/main/java/nc/ftc/inspection/dao/UsersDAO.java
@@ -0,0 +1,92 @@
+package nc.ftc.inspection.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import nc.ftc.inspection.Server;
+
+
+public class UsersDAO {
+	
+	static final String PASSWORD_SQL = "SELECT hashedPassword FROM users where username = ?";
+	static final String UPDATE_PASSWORD_SQL = "UPDATE users SET hashedPassword = ? WHERE username = ? AND hashedPassword = ?";
+	static final String NEW_USER_SQL = "INSERT INTO users VALUES (?,?,?)";
+	
+	/**
+	 * Looks up the hashed password for a given user.
+	 * @param username The username to retrieved password for.
+	 * @return The hashed password, or null if no username.
+	 */
+	public String getHashedPassword(String username){
+		try(Connection conn = DriverManager.getConnection(Server.GLOBAL_DB)){
+			PreparedStatement ps = conn.prepareStatement(PASSWORD_SQL);
+			ps.setString(1, username);
+			ResultSet rs = ps.executeQuery();
+			if(!rs.next()){
+				return null;
+			}
+			String hash = rs.getString(1);
+			return hash;
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * Updates the password of a given user.
+	 * @param username The username to edit
+	 * @param oldHashedPw The current hashed pw, used to verify
+	 * @param newHashedPw The hash of the new password
+	 * @return True if successful, false if failed (due to either no username found or incorrect current username).
+	 */
+	public boolean updatePassword(String username, String oldHashedPw, String newHashedPw){
+		try(Connection conn = DriverManager.getConnection(Server.GLOBAL_DB)){
+			PreparedStatement ps = conn.prepareStatement(UPDATE_PASSWORD_SQL);
+			ps.setString(1, newHashedPw);
+			ps.setString(2, username);
+			ps.setString(3, oldHashedPw);
+			int affected = ps.executeUpdate();
+			if(affected > 1){
+				throw new RuntimeException("OMFG WE HAD >1 USER ENTRIES -- WE DONE SCREWED UP");
+			}
+			return affected == 1;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param username
+	 * @param hashedPw
+	 * @param type
+	 * @return
+	 */
+	public boolean addUser(String username, String hashedPw, int type){
+		try(Connection conn = DriverManager.getConnection(Server.GLOBAL_DB)){
+			PreparedStatement ps = conn.prepareStatement(NEW_USER_SQL);
+			ps.setString(1, username);
+			ps.setString(2, hashedPw);
+			ps.setInt(3, type);
+			int affected = ps.executeUpdate(); //Existing user will throw SQL exception for PK violation
+			if(affected > 1){
+				throw new RuntimeException("WTF?");
+			}
+			return affected == 1;
+		}catch(Exception e){
+			if(e instanceof SQLException){
+				if(e.getMessage().contains("[SQLITE_CONSTRAINT]")){
+					return false;
+				}
+			}
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+}
